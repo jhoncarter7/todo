35683d20a0402f00abfa3d6f612717c6e9011bea
jhoncarter7 <rainikhil044@gmail.com>
2023-12-12 12:21:26 +0530

    complete edit and update functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import { Route,  Routes } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import Todo from "./pages/Todo";
-import SignIn from "./pages/signin";
+import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import PrivateRoute from "./component/PrivateRoute";
+import AddTodo from "./pages/AddTodo";
+import EditTodo from "./pages/EditTodo";
 
 export default function App() {
   return (
@@ -11,6 +13,8 @@ export default function App() {
       <Routes>
         <Route element={<PrivateRoute/>}>
         <Route path="/" element={<Todo/>}/>
+        <Route path="/addTodo" element={<AddTodo/>}/>
+        <Route path="/editTodo/:id" element={<EditTodo/>}/>
         </Route>
         <Route path="/signin" element={<SignIn/>}/>
         <Route path="/signup" element={<SignUp/>}/>
--- client/src/pages/Todo.jsx
+++ client/src/pages/Todo.jsx
@@ -1,22 +1,54 @@
-import classes from './Todo.module.css'
+import { useEffect, useState } from "react";
+import classes from "./Todo.module.css";
 import { MdEditNote } from "react-icons/md";
 import { MdDeleteForever } from "react-icons/md";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Todo() {
-
-   
+  const [userTodos, setUsertodos] = useState([])
+  const {currentUser} = useSelector((state) => state.user)
+ 
+  useEffect(() => {
+    const fetchTodos = async ()=>{
+      try {
+        const res = await fetch(`/api/todo/getTodos/${currentUser.uid}`)
+        const data = await res.json()
+        if(data.success === false){
+          console.log(data.message)
+          return;
+        }
+        setUsertodos(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchTodos()
+  },[currentUser.uid])
+ 
   return (
     <div className={classes.todo}>
-        <h2> My Todo list</h2>
-        <div className={classes.todoItems}>
-            <div className={classes.todoItem}>
-            <p>Complete Bca projects</p>
+      <h2> My Todo list</h2>
+      <div className={classes.todoItems}>
+        {
+        userTodos && userTodos.map((todo)=> (
+            <div className={classes.todoItem} key={todo._id}>
+            <p>{todo.title}</p>
             <div>
-            <MdEditNote className={classes.edit}/>
-            <MdDeleteForever className={classes.delete}/>
+              <Link to={`/editTodo/${todo._id}`}>
+              <MdEditNote className={classes.edit} />
+              </Link>
+              
+              <MdDeleteForever className={classes.delete} />
             </div>
-            </div>
-        </div>
+          </div>
+          ))
+        }
+      
+      </div>
+      <Link to={"/addTodo"}>
+        <div className={classes.addTodo}>add new Todo</div>
+      </Link>
     </div>
-  )
+  );
 }
